662f4de88ad04a744454cfb632c7f58cb9fdab86
rachoac <aron.racho@gmail.com>
2017-10-12 21:48:29 -0700

    client

--- client/start.sh
+++ client/start.sh
@@ -1,3 +1,12 @@
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  REFLEX_CMD="./reflex"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  REFLEX_CMD="./reflex-osx"
+else
+    echo "Unsupported operating system."
+    exit 1
+fi
+
 if [ -z "$1" ]; then
 	echo "You must specify your minecraft name!"
 	exit 1
@@ -27,4 +36,5 @@ echo "Place your .js scripts in $SCRIPT_DIR/$1."
 export M_USER=$1
 export M_SERVER=$2
 export JS_SECRET=$3
-reflex -g "scripts/$1/*.js" -- sh -c './ship.sh {}'
+
+$REFLEX_CMD -g "scripts/$1/*.js" -- sh -c './ship.sh {}'
