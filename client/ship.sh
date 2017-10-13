0be184874852bc7ee598a8893ea36a2938a4d63b
rachoac <aron.racho@gmail.com>
2017-10-12 21:24:33 -0700

    client

--- client/ship.sh
+++ client/ship.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-THE_FILE=$1
+THE_FULL_FILE=$1
+THE_FILE=$(echo $THE_FULL_FILE | awk -F"/" '{print $3}')
 echo "SHIPPING $THE_FILE... $M_USER"
-SCRIPT=$(base64 <<< cat $THE_FILE)
+SCRIPT=$(base64 <<< cat $THE_FULL_FILE)
 echo $SCRIPT
-curl "http://ec2-35-161-221-235.us-west-2.compute.amazonaws.com:3000/script/$M_USER/$THE_FILE?secret=$JS_SECRET" -H 'Content-type: application/x-www-form-urlencoded' -X POST -d "script=$SCRIPT"
+CURL_CMD="$M_SERVER:3000/script/$M_USER/$THE_FILE?secret=$JS_SECRET"
+curl "$CURL_CMD" -H 'Content-type: application/x-www-form-urlencoded' -X POST -d "script=$SCRIPT" --data-urlencode -i
--- client/start.sh
+++ client/start.sh
@@ -4,12 +4,27 @@ if [ -z "$1" ]; then
 fi
 
 if [ -z "$2" ]; then
+    echo "You must specify the server!"
+    exit 1
+fi
+
+if [ -z "$3" ]; then
     echo "You must specify the server secret!"
     exit 1
 fi
 
+if [ ! -d "scripts" ]; then
+    mkdir "scripts"
+fi
 
+if [ ! -d "scripts/$1" ]; then
+    mkdir "scripts/$1"
+fi
+
+SCRIPT_DIR=$(pwd)/scripts
+echo "Place your .js scripts in $SCRIPT_DIR/$1."
 
 export M_USER=$1
-export JS_SECRET=$2
-reflex -g '*.js' -- sh -c './ship.sh {}'
+export M_SERVER=$2
+export JS_SECRET=$3
+reflex -g "scripts/$1/*.js" -- sh -c './ship.sh {}'
