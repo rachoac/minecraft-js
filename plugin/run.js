b9b365ddced1b6419026e25ebeef8b789521a9a4
rachoac <aron.racho@gmail.com>
2017-10-21 12:42:36 -0700

    work

--- plugin/run.js
+++ plugin/run.js
@@ -1,13 +1,21 @@
 var request = require('http/request');
+echo("run() version 1")
 exports.run = function(script) {
+    const startingIndex = script ? 1 : 0
     if (!script) {
-        script = 'current.js'
+        script = 'current.js';
     }
+
+    const args = []
+    for ( var i = startingIndex; i < arguments.length; i++ ){
+        args.push(arguments[i])
+    }
+
     var scriptName = '__current_' + self.name;
-    request.request('http://localhost:3000/script/' + script + '?scriptName=' + scriptName,
+    request.request('http://localhost:3000/script/' + self.name + '/' + script + '?scriptName=' + scriptName,
         function(c, body) {
             echo(body);
             eval(body);
-            eval(scriptName + '();');
+            eval(scriptName + '.apply(this, args);');
         });
 }
